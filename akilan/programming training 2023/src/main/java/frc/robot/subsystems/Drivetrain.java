3124cba452a575fbf904708c5d68556ef404d241
Akilan Paramasivam <bpakilan@gmail.com>
2023-09-21 18:01:28 -0700

    Hi joystick whatever

--- akilan/programming training 2023/src/main/java/frc/robot/subsystems/Drivetrain.java
+++ akilan/programming training 2023/src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -4,14 +4,47 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+
 public class Drivetrain extends SubsystemBase {
+  private static class Config {
+    public static final int kRightPrimaryMotorID = 1;
+    public static final int kLeftPrimaryMotorID = 2;
+    public static final int kRightSecondaryMotorID = 3;
+    public static final int kLeftSecondaryMotorID = 4;
+  }
+  private WPI_TalonFX m_rightPrimary = new WPI_TalonFX(Config.kRightPrimaryMotorID);
+  private WPI_TalonFX m_leftPrimary = new WPI_TalonFX(Config.kLeftPrimaryMotorID);
+  private WPI_TalonFX m_rightSecondary = new WPI_TalonFX(Config.kRightSecondaryMotorID);
+  private WPI_TalonFX m_leftSecondary = new WPI_TalonFX(Config.kLeftSecondaryMotorID);
   /** Creates a new Drivetrain. */
-  public Drivetrain() {}
+  public Drivetrain() {
+    m_leftSecondary.follow(m_leftPrimary);
+    m_rightSecondary.follow(m_rightPrimary);
+
+    m_rightPrimary.setInverted(true);
+    m_rightSecondary.setInverted(true);
+  }
+  public void setRightSpeed(double rightSpeed){
+    m_rightPrimary.set(rightSpeed);
+  }
+  public void setLeftSpeed(double leftSpeed){
+    m_leftPrimary.set(leftSpeed);
+  }
+
+  public void setIdleMode(NeutralMode idleMode){
+    m_leftPrimary.setNeutralMode(idleMode);
+    m_rightPrimary.setNeutralMode(idleMode);
+    m_leftSecondary.setNeutralMode(idleMode);
+    m_rightSecondary.setNeutralMode(idleMode);
+  }
+
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-    //hello
   }
-}
+} 
